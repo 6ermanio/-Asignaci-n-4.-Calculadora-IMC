ffc0d92e629bbffacd9a7484163812dec8179bfb
6ermanio <92658165+6ermanio@users.noreply.github.com>
2023-02-05 21:36:17 -0700

    Add files via upload

---
+++ Asignacion4_CalculadoraIMC_Verdugo_German/app/src/main/java/verdugo/german/asignacion4_calculadoraimc_verdugo_german/MainActivity.kt
@@ -0,0 +1,60 @@
+package verdugo.german.asignacion4_calculadoraimc_verdugo_german
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+    var indice: Float = 0F
+    var peso: Int = 0
+    var altura: Float = 0F
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val weight: EditText = findViewById(R.id.etPeso)
+        val height: EditText = findViewById(R.id.etEstatura)
+        val imc: TextView = findViewById(R.id.tvImc)
+        val range: TextView = findViewById(R.id.tvRange)
+        val calcular: Button = findViewById(R.id.btnCalcular)
+
+        calcular.setOnClickListener {
+            peso = weight.getText().toString().toInt()
+            altura = height.getText().toString().toFloat()
+
+            indice = peso/(altura.pow(2))
+
+            if(indice<18.5f){
+                imc.setText(indice.toString())
+                range.setText("Bajo Peso")
+                range.setBackgroundResource(R.color.colorBrown)
+            }else if(indice in 18.5f..24.9f){
+                imc.setText(indice.toString())
+                range.setText("Normal")
+                range.setBackgroundResource(R.color.colorGreen)
+            }else if(indice in 25f..29.9f){
+                imc.setText(indice.toString())
+                range.setText("Sobrepeso")
+                range.setBackgroundResource(R.color.colorGreenish)
+            }else if(indice in 30f..34.9f){
+                imc.setText(indice.toString())
+                range.setText("Obesidad Grado 1")
+                range.setBackgroundResource(R.color.colorYellow)
+            }else if(indice in 35f..39.9f){
+                imc.setText(indice.toString())
+                range.setText("Obesidad Grado 2")
+                range.setBackgroundResource(R.color.colorOrange)
+            }else if(indice>=40){
+                imc.setText(indice.toString())
+                range.setText("Obesidad Grado 3")
+                range.setBackgroundResource(R.color.colorRed)
+            }
+        }
+    }
+}
